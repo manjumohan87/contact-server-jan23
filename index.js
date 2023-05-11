8e31d9c3168cb52d0ccb1c63bf090c450ace600a
manjumohan87 <manjumohan87@gmail.com>
2023-05-11 11:54:59 +0530

    contact server jan23

---
+++ index.js
@@ -0,0 +1,29 @@
+// import json-server
+
+const jsonServer = require('json-server')
+
+// create json server application
+
+const server = jsonServer.create()
+
+// set up route for db.json
+
+const router = jsonServer.router("db.json")
+
+//return a middleware used by JSON Server
+
+const middleware = jsonServer.defaults()
+
+//set up  port number for server app
+
+const port = process.env.PORT || 4000
+
+//use router and middleware in server app
+server.use(middleware)
+server.use(router)
+
+// to listen the server app in port
+
+server.listen(port,()=>{
+    console.log(`Contact sever app started at port ${port}`);
+})
